c4a6f81646ffd5d5e05edc2406f04a9aa4754c55
Kriti Kalpa <krtklpsh@gmail.com>
2020-09-29 19:50:10 +0530

    [Kriti] Add. Movement conditions added

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -6,14 +6,35 @@ namespace SnakeAndLadder
     {
         static void Main(string[] args)
         {
+            const int LADDER = 1;
+            const int SNAKE = 2;
+
             int playerPosition = 0;
 
             Random random = new Random();
             int dice = random.Next(1, 7);
+            int movement = random.Next(0, 3);
 
             Console.WriteLine("Number on the dice : " + dice);
 
-            playerPosition += dice;
+            switch (movement)
+            {
+                case LADDER:
+                    Console.WriteLine("Player climbs the ladder");
+                    playerPosition += dice;
+                    break;
+
+                case SNAKE:
+                    Console.WriteLine("Player slides down the snake");
+                    playerPosition -= dice;
+                    break;
+
+                default:
+                    Console.WriteLine("No-Play by the player");
+                    break;
+
+            }
+
 
             Console.WriteLine("Player is at : " + playerPosition);
         }
