f2b5659045759b37decf7ace6a54705c5553fc2b
Kriti Kalpa <krtklpsh@gmail.com>
2020-09-29 19:34:00 +0530

    [Kriti] Add. Dice roll added

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -8,6 +8,13 @@ namespace SnakeAndLadder
         {
             int playerPosition = 0;
 
+            Random random = new Random();
+            int dice = random.Next(1, 7);
+
+            Console.WriteLine("Number on the dice : " + dice);
+
+            playerPosition += dice;
+
             Console.WriteLine("Player is at : " + playerPosition);
         }
     }
